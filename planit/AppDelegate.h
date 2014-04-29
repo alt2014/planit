a2d8ba752ea00cae9f844e8c6cd92bdd18f1f2f2
alt2014 <anhlt92@stanford.edu>
2014-04-29 07:19:10 -0700

    initial commit

---
+++ planit/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  planit
+//
+//  Copyright (c) 2014 Anh Truong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
